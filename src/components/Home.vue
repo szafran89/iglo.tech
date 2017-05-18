413672f779d91adfec5a73780c17574dbeebca93
Bartek Igielski <igloczek@gmail.com>
2017-05-18 17:28:55 +0200

    Home and Posts components added

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h1>
+            Home
+        </h1>
+        <posts></posts>
+    </section>
+</template>
+
+<script>
+    import Posts from '@/components/Posts'
+
+    export default {
+        name: '',
+        components: {
+            Posts
+        }
+    }
+</script>
---
+++ src/components/Posts.vue
@@ -0,0 +1,56 @@
+<template>
+    <ul class="post-list">
+        <li class="post-list__item" v-for="post in posts">
+            <router-link :to="cutUrl(post.link)" replace>
+                <h2>
+                    {{ post.title.rendered }}
+                </h2>
+            </router-link>
+            <p  v-html="post.content.rendered "></p>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'Posts',
+        data() {
+            return {
+                posts: []
+            }
+        },
+        methods: {
+            getPosts() {
+                this.$http.get('http://snowdog.dev/wp-json/wp/v2/posts')
+                    .then(response => {
+                        this.posts = response.body;
+                    });
+            },
+            cutUrl(url) {
+                return url.replace('http://snowdog.dev/', '');
+            }
+        },
+        created() {
+            this.getPosts()
+        }
+    }
+
+</script>
+
+<style>
+    .post-list {
+        list-style: none;
+        text-align: left;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    .post-list__item {
+        width: 20%;
+        padding: 20px;
+    }
+    .post-list__item img {
+        max-width: 100%;
+        height: auto;
+    }
+</style>

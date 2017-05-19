b7870f3aaa38e99c9e599a292aafb37af93d5468
Bartek Igielski <igloczek@gmail.com>
2017-05-19 13:00:41 +0200

    Routing based on custom Wordpress API endpoint

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Resource from 'vue-resource'
+
 import Home from '@/components/Home'
+import Post from '@/components/Post'
+import Page from '@/components/Page'
+import NotFound from '@/components/404'
 
 Vue.use(Router)
+Vue.use(Resource)
 
-export default new Router({
+const api = 'http://snowdog.dev/wp-json/snowdog/type-by-url'
+
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -14,3 +22,27 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.length === 0) {
+    Vue.http.get(`${api}${to.path}`)
+      .then(response => {
+        switch(response.body) {
+          case 'post':
+            router.addRoutes([{path: to.path, component: Post}])
+          case 'page':
+            router.addRoutes([{path: to.path, component: Page}])
+          default:
+            router.addRoutes([{path: to.path, component: NotFound}])
+        }
+        next(to.path)
+        return
+      })
+  }
+  else {
+    next()
+  }
+})
+
+export default router
+

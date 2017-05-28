12e0c7b7236c44acf5bd6f64da27655001e49c2f
Bartek Igielski <igloczek@gmail.com>
2017-05-28 02:13:58 +0200

    Updated router to display 404 and use global url config

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Resource from 'vue-resource'
+import Backend from '../../config/backend'
 
 import Home from '@/components/Home'
 import Post from '@/components/Post'
-import Page from '@/components/Page'
 import NotFound from '@/components/404'
 
 Vue.use(Router)
 Vue.use(Resource)
-
-const api = 'http://snowdog.dev/wp-json/snowdog/type-by-url'
 
 const router = new Router({
   mode: 'history',
@@ -25,16 +23,20 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   if (to.matched.length === 0) {
-    Vue.http.get(`${api}${to.path}`)
+    Vue.http.get(`${Backend()}/wp-json/headless/v1/type-by-url${to.path}`)
       .then(response => {
         switch(response.body) {
           case 'post':
-            router.addRoutes([{path: to.path, component: Post}])
           case 'page':
-            router.addRoutes([{path: to.path, component: Page}])
+            router.addRoutes([{path: to.path, component: Post}])
           default:
             router.addRoutes([{path: to.path, component: NotFound}])
         }
+      })
+      .catch(() => {
+        router.addRoutes([{path: to.path, component: NotFound}])
+      })
+      .finally(() => {
         next(to.path)
         return
       })

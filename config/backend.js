ae91ef9e111a6d2597de8205048df424bcf8ae6c
Bartek Igielski <igloczek@gmail.com>
2017-05-28 14:45:41 +0200

    Backend config update

--- config/backend.js
+++ config/backend.js
@@ -1,6 +1,3 @@
 module.exports = function() {
-    if (process.env.NODE_ENV === 'production') {
-        return 'https://iglo.tech'
-    }
-    return 'http://snowdog.dev'
+    return 'https://iglo.tech'
 }

61310220756b555f9ff13b8fca2fa714e22d8fe0
Bartek Igielski <igloczek@gmail.com>
2017-05-28 02:15:19 +0200

    Post component update

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,11 +1,18 @@
 <template>
-    <section>
-        <h1>Post</h1>
-        <article v-if="post" v-html="post.post_content"></article>
+    <section class="post">
+        <time class="post__date">
+            {{ parseDate(post.post_date) }}
+        </time>
+        <h1 class="post__heading">
+            {{ post.post_title }}
+        </h1>
+        <article v-html="post.post_content" class="post__content"></article>
     </section>
 </template>
 
 <script>
+    import Backend from '../../config/backend'
+
     export default {
         data() {
             return {
@@ -14,14 +21,75 @@
         },
         methods: {
             getPost() {
-                this.$http.get(`http://snowdog.dev/wp-json/snowdog/post-by-url${this.$route.path}`)
+                this.$http.get(`${Backend()}/wp-json/headless/v1/post-by-url${this.$route.path}`)
                     .then(response => {
                         this.post = response.body;
                     });
+            },
+            parseDate(date) {
+                return new Date(Date.parse(date)).toLocaleString('en-GB', { year: 'numeric', month: 'long', day: '2-digit' })
             }
         },
         created() {
             this.getPost()
+        },
+        watch: {
+            '$route' (to, from) {
+                this.getPost()
+            }
         }
     }
 </script>
+
+<style lang="scss">
+    .post {
+        padding: 20px 0;
+        &__date {
+            font-size: 12px;
+            text-transform: uppercase;
+        }
+        &__heading {
+            font-weight: bold;
+            margin: 0;
+        }
+        &__content {
+            margin: 20px 0;
+            line-height: 1.5;
+            a {
+                color: #3498db;
+                font-weight: bold;
+                text-decoration: none;
+                transition: color 300ms ease-in-out;
+                &:hover {
+                    color: #2980b9;
+                }
+                &:visited {
+                    color: #9b59b6;
+                    &:hover {
+                        color: #8e44ad;
+                    }
+                }
+            }
+            blockquote {
+                position: relative;
+                margin: 10px 0;
+                padding: 10px 20px;
+                &:before {
+                    content: '';
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    height: 100%;
+                    width: 10px;
+                    background: #eee;
+                }
+            }
+            pre {
+                width: 100%;
+                padding: 20px;
+                overflow: scroll;
+                background: #eee;
+            }
+        }
+    }
+</style>
